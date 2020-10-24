4e1a4de7731285ea7259044b0818316bce36e9de
Rashed <rashed.addosary@gmail.com>
2020-10-24 09:34:44 +0300

    adding the password confirmation

--- src/contracts/requests/PostRegister.request.ts
+++ src/contracts/requests/PostRegister.request.ts
@@ -4,6 +4,7 @@ export class PostRegisterRequest {
     email: string;
     name: string;
     password: string;
+    confirmPassword: string;
     firm: PostRegisterRequestFirm;
 }
 
@@ -16,6 +17,7 @@ export const PostRegisterRequestSchema = yup.object<PostRegisterRequest>({
     email: yup.string().email().required(),
     name: yup.string().required(),
     password: yup.string().required(),
+    confirmPassword: yup.string().oneOf([yup.ref('password')]).required(),
     firm: yup.object<PostRegisterRequestFirm>({
         name: yup.string().required(),
         logoUrl: yup.string().optional()
